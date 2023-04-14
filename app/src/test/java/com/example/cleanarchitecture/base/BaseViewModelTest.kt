fd3afcb589f8e418829457002f64ecf1824092fb
ron <ron@akaai.kr>
2023-04-14 16:50:40 +0900

    test dependencies

---
+++ app/src/test/java/com/example/cleanarchitecture/base/BaseViewModelTest.kt
@@ -0,0 +1,8 @@
+package com.example.cleanarchitecture.base
+
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class BaseViewModelTest {
+}
